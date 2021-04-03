477b3f429fb2d22ab3e4c376d3f665bedde2b228
George Dilthey <george.dilthey@gmail.com>
2021-04-03 12:30:22 -0400

    commit tree 7ac8ec65210bbf73763d3d10309c6163f91b9de7

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4,"M", "Black", "Happy", "true");
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3,"F", "Black", "Sad", "true")
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8,"F", "Brown", "Gleeful", "true")
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Seargeant Brown", 4,"M", "Black", "Happy", "true")
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4,"M", "Black", "Happy", "true")
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4,"M", "Black", "Happy", "true")
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4,"M", "Black", "Happy", "true")
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4,"M", "Black", "Happy", "true")
